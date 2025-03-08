92e0a730fad9fcc42dd4065c75958576bc6edc4c
María Andrea Méndez <101602812+mariaa-mendez@users.noreply.github.com>
2025-03-08 14:05:42 -0500

    Update ServicioControlador.java

--- providers/src/main/java/com/iwellness/providers/Controlador/ServicioControlador.java
+++ providers/src/main/java/com/iwellness/providers/Controlador/ServicioControlador.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,7 +44,7 @@ try {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error al guardar el servicio: " + e.getMessage());
         }    }
 
-    @GetMapping("/update/{servicio}")
+    @PutMapping("/update/{servicio}")
     public ResponseEntity<?> Actualizar(@PathVariable Servicio servicio){
         try {
             return ResponseEntity.ok(servicioServicio.Actualizar(servicio));
