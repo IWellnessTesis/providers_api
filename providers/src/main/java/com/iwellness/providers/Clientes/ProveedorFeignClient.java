32a63ef224065cbc153fb307b1266a1e4515ca51
clarck2547 <63880026+clarck2547@users.noreply.github.com>
2025-03-27 21:20:56 -0500

    fix: correcion rutas

--- providers/src/main/java/com/iwellness/providers/Clientes/ProveedorFeignClient.java
+++ providers/src/main/java/com/iwellness/providers/Clientes/ProveedorFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.iwellness.providers.DTO.ProveedorDTO;
 
-@FeignClient(name = "proveedor-ms", url = "http://localhost:8082/auth")
+@FeignClient(name = "proveedor-ms", url = "http://localhost:8082/usuarios")
 public interface ProveedorFeignClient {
 
     @GetMapping("/buscar/{idUsuario}")
